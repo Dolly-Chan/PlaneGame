74e3415087f33ced1cc15e26914edee8b2661011
Dolly-Chan <76759618+Dolly-Chan@users.noreply.github.com>
2021-03-17 15:18:40 +0800

    Update plane_sprites.py

--- plane_sprites.py
+++ plane_sprites.py
@@ -1,1 +1,14 @@
+import pygame
 
+SCREEN_RECT = pygame.Rect(0,0,700,500)
+
+class GameSprites(pygame.sprites.Sprites):
+  
+  def __int__(self, image_name, speed=1):
+    super().__init__()
+    
+    self.image = pygame.image.load(image_name)
+    self.rect = self.image.get_rect()
+    self.speed = speed
+    
+  def update(self):
